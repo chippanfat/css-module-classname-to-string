d6bb4cb8587f90ca0b1464c6cbe3163fd738d695
Ethan Flynn-Harding <efh@deliowealth.com>
2022-05-20 11:00:14 +0100

    re-writing className props

--- src/components.ts
+++ src/components.ts
@@ -1,14 +1,74 @@
 // eslint-disable-next-line node/no-missing-import
-import { readdir, readFile } from "node:fs/promises";
+import { readdir, readFile, writeFile } from "node:fs/promises";
 
 export default class Components {
   private readonly extensions: string[] = ["ts"];
   private readonly styleExtensions: string[] = ["css", "less", "scss"];
+
   constructor(private readonly folderList: string[]) {}
 
-  async handleFile(file: string, styleModule: string): Promise<void> {
+  private static async convertClassNamesToString(
+    file: string,
+    styleModule: string
+  ): Promise<void> {
+    const content = await Components.getFileContent(file);
+    const newFile: string[] = [];
+    let shouldWriteFile = false;
+
+    for (const line of content) {
+      const regex = new RegExp(
+        "className=\\{(" + styleModule + ".([a-zA-Z]+))}"
+      );
+      const matcher = regex.exec(line);
+
+      if (matcher === null) {
+        newFile.push(line);
+      } else {
+        shouldWriteFile = true;
+        newFile.push(line.replace(`{${matcher[1]}}`, `"${matcher[2]}"`));
+      }
+    }
+
+    if (shouldWriteFile) {
+      await writeFile(file, newFile.join("\n"), {
+        encoding: "utf8",
+        flag: "w+",
+      });
+    }
+  }
+
+  private static async getFileContent(file: string): Promise<string[]> {
     const content = await readFile(file, "utf8");
-    console.log(content.split("\n"));
+    return content.split("\n");
+  }
+
+  private static async getStyleImport(
+    file: string,
+    styleModule: string
+  ): Promise<RegExpExecArray> {
+    const content = await Components.getFileContent(file);
+    for (const line of content) {
+      const regex = new RegExp(
+        "import\\s([a-zA-Z]+)\\sfrom\\s[\\'|\\\"].*(" +
+          styleModule +
+          ")[\\'|\\\"]",
+        ""
+      );
+
+      const matcher = regex.exec(line);
+      if (matcher !== null) {
+        return matcher;
+      }
+    }
+
+    throw new Error("Unable to find style import");
+  }
+
+  private removeStyleImportFromComponent() {}
+
+  async handleFile(file: string, styleModule: string): Promise<void> {
+    const styles = await Components.getStyleImport(file, styleModule);
+    await Components.convertClassNamesToString(file, styles[1]);
   }
 
   private getStyleModuleForComponent = (files: string[]) => {
